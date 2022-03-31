947b3124682d795a462aed3e7bd130f08cc87af1
Alzound <aaxlcd@gmail.com>
2022-03-30 22:53:05 -0600

    Práctica2

---
+++ Alzound/JS/Script1.js
@@ -0,0 +1,59 @@
+const games = ["WhatWeWere", "CerealServed", "Mind&Bunny", "YourTravellingGuide"];
+
+function DateTime()
+{
+    onclick=document.getElementById('dateTime').innerHTML = Date();
+    id ="dateTime";
+    alert("Date&Time"); 
+   
+}
+
+function Greetings()
+{
+    if (new Date().getHours() < 18) 
+    {
+        alert("Good day to be more, Comrade!");
+        //document.getElementById("demo").innerHTML = "Good day Comrade!"; 
+    }
+    else
+    {
+        alert("Good Evening may be upon you, Comrade!");
+        //document.getElementById("demo").innerHTML = "Good Evening Comrade!";
+    }
+    
+}
+
+function ChangeDataGames()
+{
+    
+    let text = "";
+    for (let i = 0; i < games.length; i++) 
+    {
+        text += games[i] + "<br>";
+        setInterval(() => 5000);
+        document.getElementById("games").innerHTML = text;
+    }
+    
+    alert(entro);     
+}
+
+function WhileData()
+{
+  let text = "";
+  let i = 0;
+  while (i < 10) {
+  text += "<br>The number is " + i;
+  i++;
+}
+document.getElementById("while").innerHTML = text;
+}
+
+
+
+
+
+function Life()
+{
+    document.write(40+2);
+    alert("The number that solve's all your problems"); 
+}
